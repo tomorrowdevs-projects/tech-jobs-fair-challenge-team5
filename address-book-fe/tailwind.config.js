6f9e01edb6b52be4086405aecae627ddba4d7b7c
Omar Said Ibrahim <omarsaidita@gmail.com>
2024-04-04 11:01:07 +0200

    added main page

--- address-book-fe/tailwind.config.js
+++ address-book-fe/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        arimo: ["Arimo", "sans-serif"],
+      },
+      colors: {
+        primary: "#1B84B1",
+        secondary: "#A09E9E",
+        main: "#FFEBCD",
+      },
+    },
   },
   plugins: [],
 };
