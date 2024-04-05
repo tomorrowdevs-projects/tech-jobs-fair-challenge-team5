ee667356c621d1181030262be6a5391e682e5641
Luca Leandri <133882081+locabit@users.noreply.github.com>
2024-04-05 20:57:56 +0200

    desktop-trial-version

--- address-book-fe/src/components/ContactCard.tsx
+++ address-book-fe/src/components/ContactCard.tsx
@@ -1,46 +1,56 @@
-import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdOutlineEmail } from "react-icons/md"
-import { FaPhone } from "react-icons/fa6"
-import { Contact } from "../types"
-import useLongPress from "../hooks/useLongPress"
-import { Fragment, useMemo, useState } from "react"
+import {
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdOutlineEmail,
+} from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+import { Contact } from "../types";
+import useLongPress from "../hooks/useLongPress";
+import { Fragment, useMemo, useState } from "react";
 
 interface ContactProps {
-  contact: Contact
-  setSelectedCards: (value: Contact[] | undefined) => void
-  selectedCards: Contact[] | undefined
+  contact: Contact;
+  setSelectedCards: (value: Contact[] | undefined) => void;
+  selectedCards: Contact[] | undefined;
 }
 
-const ContactCard = ({ contact, setSelectedCards, selectedCards }: ContactProps) => {
+const ContactCard = ({
+  contact,
+  setSelectedCards,
+  selectedCards,
+}: ContactProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isEdit, setIsEdit] = useState<boolean>(false);
   const onLongPress = useLongPress();
 
   const isContactSelected = useMemo(() => {
-    return !!selectedCards?.find((c) => c.name === contact.name)
-  }, [contact, selectedCards])
+    return !!selectedCards?.find((c) => c.name === contact.name);
+  }, [contact, selectedCards]);
 
   const handleOnClick = () => {
     if (isContactSelected) {
       if (!!selectedCards?.length && selectedCards.length === 1) {
-        setSelectedCards(undefined)
+        setSelectedCards(undefined);
       } else {
-        const filteredCardsSelected = selectedCards?.filter((c) => c.name !== contact.name)
-        setSelectedCards(filteredCardsSelected)
+        const filteredCardsSelected = selectedCards?.filter(
+          (c) => c.name !== contact.name
+        );
+        setSelectedCards(filteredCardsSelected);
       }
     } else {
       if (!!selectedCards?.length && selectedCards.length >= 1) {
-        const currentCardsSelected = selectedCards
-        currentCardsSelected?.push(contact)
-        setSelectedCards([...currentCardsSelected])
+        const currentCardsSelected = selectedCards;
+        currentCardsSelected?.push(contact);
+        setSelectedCards([...currentCardsSelected]);
       }
     }
-  }
+  };
 
   const handleOnLongPress = () => {
     if (!selectedCards && !isOpen) {
-      setSelectedCards([contact])
+      setSelectedCards([contact]);
     }
-  }
+  };
 
   const handleSaveClick = () => {
     setIsOpen((prev) => !prev);
@@ -51,24 +61,70 @@ const ContactCard = ({ contact, setSelectedCards, selectedCards }: ContactProps)
     <div
       onClick={handleOnClick}
       {...onLongPress(handleOnLongPress)}
-      className={`flex flex-col w-full justify-between items-center p-2 gap-2 ${isContactSelected ? 'bg-primary transition duration-300 ease-in-out' : 'bg-white transition duration-300 ease-in-out'} rounded-md card-shadow`}
+      className={`flex flex-col w-full justify-between items-center p-2 gap-2 ${
+        isContactSelected
+          ? "bg-primary transition duration-300 ease-in-out"
+          : "bg-white transition duration-300 ease-in-out"
+      } rounded-md card-shadow`}
     >
       <div className="flex w-full justify-between">
         <div className="flex justify-start items-center gap-2 font-arimo">
           <img
             src={contact.avatar}
             alt={"avatar"}
-            className={`rounded-full w-[50px] h-[50px] border-[1px] bg-white ${isContactSelected ? 'border-white transition duration-300 ease-in-out' : 'border-primary transition duration-300 ease-in-out'} shadow-lg`}
+            className={`rounded-full w-[50px] h-[50px] border-[1px] bg-white ${
+              isContactSelected
+                ? "border-white transition duration-300 ease-in-out"
+                : "border-primary transition duration-300 ease-in-out"
+            } shadow-lg`}
           />
           <div className="flex flex-col">
-            <p className={`${isContactSelected ? 'text-white transition duration-300 ease-in-out' : 'text-primary transition duration-300 ease-in-out'} font-bold`}>{contact.name}</p>
+            <p
+              className={`${
+                isContactSelected
+                  ? "text-white transition duration-300 ease-in-out"
+                  : "text-primary transition duration-300 ease-in-out"
+              } font-bold`}
+            >
+              {contact.name}
+            </p>
             <div className="flex justify-start items-center gap-2 mb-1">
-              <MdOutlineEmail className={`${isContactSelected ? 'text-white transition duration-300 ease-in-out' : 'text-primary transition duration-300 ease-in-out'}`} />
-              <a href={`mailto:${contact.email}`} className={`text-xs underline ${isContactSelected ? 'text-white transition duration-300 ease-in-out' : 'text-secondary transition duration-300 ease-in-out'}`}>{contact.email}</a>
+              <MdOutlineEmail
+                className={`${
+                  isContactSelected
+                    ? "text-white transition duration-300 ease-in-out"
+                    : "text-primary transition duration-300 ease-in-out"
+                }`}
+              />
+              <a
+                href={`mailto:${contact.email}`}
+                className={`text-xs underline ${
+                  isContactSelected
+                    ? "text-white transition duration-300 ease-in-out"
+                    : "text-secondary transition duration-300 ease-in-out"
+                }`}
+              >
+                {contact.email}
+              </a>
             </div>
             <div className="flex justify-start items-center gap-2">
-              <FaPhone className={`${isContactSelected ? 'text-white transition duration-300 ease-in-out' : 'text-primary transition duration-300 ease-in-out'}`} />
-              <a href={`tel:${contact.phone}`} className={`text-xs ${isContactSelected ? 'text-white transition duration-300 ease-in-out' : 'text-secondary transition duration-300 ease-in-out'}`}>{contact.phone}</a>
+              <FaPhone
+                className={`${
+                  isContactSelected
+                    ? "text-white transition duration-300 ease-in-out"
+                    : "text-primary transition duration-300 ease-in-out"
+                }`}
+              />
+              <a
+                href={`tel:${contact.phone}`}
+                className={`text-xs ${
+                  isContactSelected
+                    ? "text-white transition duration-300 ease-in-out"
+                    : "text-secondary transition duration-300 ease-in-out"
+                }`}
+              >
+                {contact.phone}
+              </a>
             </div>
           </div>
         </div>
@@ -137,7 +193,7 @@ const ContactCard = ({ contact, setSelectedCards, selectedCards }: ContactProps)
         </Fragment>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default ContactCard
+export default ContactCard;
---
+++ address-book-fe/src/components/FiltersSection.tsx
@@ -0,0 +1,24 @@
+function FiltersSection() {
+  return (
+    <div className="flex flex-col items-center gap-4 bg-white p-4 min-w-40">
+      <p className="text-sm font-arimo font-bold text-primary">Filter by</p>
+      <select id="type" className="border-2 border-black rounded-sm w-full">
+        <option value="1">clients</option>
+        <option value="2">employees</option>
+        <option value="3">suppliers</option>
+      </select>
+      <select id="type" className="border-2 border-black rounded-sm w-full">
+        <option value="1">clients</option>
+        <option value="2">employees</option>
+        <option value="3">suppliers</option>
+      </select>
+      <select id="type" className="border-2 border-black rounded-sm w-full">
+        <option value="1">clients</option>
+        <option value="2">employees</option>
+        <option value="3">suppliers</option>
+      </select>
+    </div>
+  );
+}
+
+export default FiltersSection;
