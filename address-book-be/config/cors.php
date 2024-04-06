4a8b5cbe322b36b9692a55cadecbbcd82fbe44ab
vmigliore <v.migliore@ads.it>
2024-04-06 16:05:26 +0200

    fix: cors allowed origin

--- address-book-be/config/cors.php
+++ address-book-be/config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET','POST','PUT','DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [],
 
     'allowed_origins_patterns' => [],
 
